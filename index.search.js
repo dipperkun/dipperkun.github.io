9a743b21d19518ee0f9138a0798c0ab51853a9bd
dipperkun <dipperkun@users.noreply.github.com>
2023-03-14 22:32:42 +0000

    deploy: dipperkun/learning@5683b5bdb77681c54b353c9934bb82d00092c302

--- index.search.js
+++ index.search.js
@@ -3,5 +3,5 @@ var relearn_search_index=[{content:`计算机科学的基石
 `,description:"",tags:null,title:"操作系统",uri:"/operating-system/index.html"},{content:`工作/学习中常使用的各种工具。
 `,description:"",tags:null,title:"常用工具",uri:"/tools/index.html"},{content:`hhh
 `,description:"",tags:null,title:"First",uri:"/data-structure/first/index.html"},{content:`Lorem Ipsum.
-`,description:"",tags:null,title:"温故而之知新",uri:"/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"HTML",uri:"/tags/html/index.html"},{content:`Lorem Ipsum.
+`,description:"",tags:null,title:"温故而知新",uri:"/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"HTML",uri:"/tags/html/index.html"},{content:`Lorem Ipsum.
 `,description:"",tags:["个人网站","Markdown","HTML"],title:"Hugo",uri:"/tools/hugo/index.html"},{content:"",description:"",tags:null,title:"Markdown",uri:"/tags/markdown/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"},{content:"",description:"",tags:null,title:"个人网站",uri:"/tags/%E4%B8%AA%E4%BA%BA%E7%BD%91%E7%AB%99/index.html"}]
