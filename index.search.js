769b0326e4d61b38cf901b84d263507fc25fe145
dipperkun <dipperkun@users.noreply.github.com>
2023-03-18 08:03:22 +0000

    deploy: dipperkun/learning@8971403c7ae8201ceaab4d28d920fc04e2e599bb

--- index.search.js
+++ index.search.js
@@ -1,7 +1,7 @@
 var relearn_search_index=[{content:`计算机科学的基石
 `,description:"",tags:null,title:"数据结构与算法",uri:"/data-structure/index.html"},{content:`Linux 操作系统原理
-`,description:"",tags:null,title:"操作系统",uri:"/operating-system/index.html"},{content:`工作/学习中常使用的各种工具。
-`,description:"",tags:null,title:"常用工具",uri:"/tools/index.html"},{content:`hhh
-`,description:"",tags:null,title:"First",uri:"/data-structure/first/index.html"},{content:`Lorem Ipsum.
+`,description:"",tags:null,title:"操作系统",uri:"/operating-system/index.html"},{content:`Lorem Ipsum.
+`,description:"",tags:null,title:"计算机网络",uri:"/networking/index.html"},{content:`工作/学习中常使用的各种工具。
+`,description:"",tags:null,title:"常用工具",uri:"/tools/index.html"},{content:"引言 开始 剧情 场景1 情节1 情节2 场景2 情节3 结束 总结1 总结2 后续 ",description:"",tags:null,title:"First",uri:"/data-structure/first/index.html"},{content:`Lorem Ipsum.
 `,description:"",tags:null,title:"温故而知新",uri:"/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"HTML",uri:"/tags/html/index.html"},{content:`Lorem Ipsum.
 `,description:"",tags:["个人网站","Markdown","HTML"],title:"Hugo",uri:"/tools/hugo/index.html"},{content:"",description:"",tags:null,title:"Markdown",uri:"/tags/markdown/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"},{content:"",description:"",tags:null,title:"个人网站",uri:"/tags/%E4%B8%AA%E4%BA%BA%E7%BD%91%E7%AB%99/index.html"}]
